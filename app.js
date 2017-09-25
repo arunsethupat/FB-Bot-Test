334aede873a6825559121fa910b7295a8aa35ad6
Arun Sethupat <sethupat.arun.4@gmail.com>
2017-09-25 01:13:06 -0500

    trial

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ app.use(express.static(__dirname + '/public'));
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.render('pages/index');
+  response.render("Hello World");
   console.log("Hello World");
 });
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
